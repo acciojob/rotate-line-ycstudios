34821aced557814a77aa80bf2c8757bd6293cc5b
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-17 21:42:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+const line=document.getElementById('line')
+let angle=0
+function rotation(){
+    angle+=2;
+    line.style.transform=`rotate(${angle}deg)`;
+    
+}
+setInterval(rotation, 20);//your JS code here. If required.
+const line=document.getElementById('line')
+let angle=0
+function rotation(){
+    angle+=2;
+    line.style.transform=`rotate(${angle}deg)`;
+    
+}
+setInterval(rotation, 20);
